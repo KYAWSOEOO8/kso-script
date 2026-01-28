e4526bfe6b9001c02d7d2408cc54afdbdaa883cd
KYAWSOEOO8 <148742752+KYAWSOEOO8@users.noreply.github.com>
2026-01-28 14:29:47 +0700

    Update kso-udp.sh

--- kso-udp.sh
+++ kso-udp.sh
@@ -179,7 +179,7 @@ CONFIG_FILE = "/etc/zivpn/config.json"
 LISTEN_FALLBACK = "5667"
 RECENT_SECONDS = 120
 
-LOGO_URL = "https://raw.githubusercontent.com/KYAWSOEOO8/kso-script/blob/main/icon.png"
+LOGO_URL = "https://raw.githubusercontent.com/KYAWSOEOO8/kso-script/main/icon.png"
 
 HTML = """<!doctype html>
 <html lang="my"><head><meta charset="utf-8">
@@ -688,7 +688,7 @@ CONFIG_FILE = "/etc/zivpn/config.json"
 LISTEN_FALLBACK = "5667"
 RECENT_SECONDS = 120
 
-LOGO_URL = "https://raw.githubusercontent.com/KYAWSOEOO8/kso-script/blob/main/icon.png"
+LOGO_URL = "https://raw.githubusercontent.com/KYAWSOEOO8/kso-script/main/icon.png"
 
 HTML = """<!doctype html>
 <html lang="my"><head><meta charset="utf-8">
